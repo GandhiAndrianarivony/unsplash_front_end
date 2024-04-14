c0f5290221c547c9790871d72837a013dc3a2965
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-04-14 21:12:48 +0300

    change a to Link

--- src/components/Header/RightMenu.tsx
+++ src/components/Header/RightMenu.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import PropsType from "./type";
 
 function RightMenu({ login }: PropsType) {
@@ -9,74 +11,90 @@ function RightMenu({ login }: PropsType) {
         setMenuOpen(!menuOpen);
         setMenuIcon(menuOpen ? "menu.svg" : "close.svg");
     };
+
     const loginText = !login ? (
-        <a
-            href="#"
+        <Link
+            to="#"
             className="block text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
         >
             Log in
-        </a>
+        </Link>
     ) : (
         ""
     );
-    return (
-        <div>
-            <div className="hidden md:flex space-x-4 pt-2 px-4">
-                <a
-                    href="#"
-                    className="text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
-                    aria-current="page"
+
+    const mainContent = (
+        <div className="hidden md:flex space-x-4 pt-2 px-4">
+            <Link
+                className="text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
+                aria-current="page"
+                to="#"
+            >
+                Explore
+            </Link>
+
+            <Link
+                className="text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
+                to="#"
+            >
+                Advertise
+            </Link>
+
+            {loginText}
+
+            <Link
+                to="#"
+                className="text-gray-500 hover:text-gray-700 px-3 font-normal border-2 border-gray-400 hover:border-gray-800 rounded-lg py-2"
+            >
+                Submit a photo
+            </Link>
+        </div>
+    );
+
+    // When screen size change
+    const responsiveContent = (
+        <div className="relative">
+            <button
+                type="button"
+                onClick={toggleMenu}
+                className="block md:hidden px-4"
+            >
+                <img src={menuIcon} alt="" />
+            </button>
+            <div
+                className={`absolute right-0 mt-2 shadow-md rounded-md p-4 w-48 bg-gray-200 ${
+                    menuOpen ? "block" : "hidden"
+                }`}
+            >
+                <Link
+                    to="#"
+                    className="block text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
                 >
                     Explore
-                </a>
-                <a
-                    href="#"
-                    className="text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
+                </Link>
+
+                <Link
+                    to="#"
+                    className="block text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
                 >
                     Advertise
-                </a>
+                </Link>
+
                 {loginText}
-                <a
-                    href="#"
-                    className="text-gray-500 hover:text-gray-700 px-3 font-normal border-2 border-gray-400 hover:border-gray-800 rounded-lg py-2"
+                <Link
+                    to="#"
+                    className="block text-gray-500 hover:text-gray-700 px-3 font-normal border-2 border-gray-400 hover:border-gray-800 rounded-lg py-2"
                 >
                     Submit a photo
-                </a>
+                </Link>
             </div>
-            <div className="relative">
-                <button
-                    type="button"
-                    onClick={toggleMenu}
-                    className="block md:hidden px-4"
-                >
-                    <img src={menuIcon} alt="" />
-                </button>
-                <div
-                    className={`absolute right-0 mt-2 shadow-md rounded-md p-4 w-48 bg-gray-200 ${
-                        menuOpen ? "block" : "hidden"
-                    }`}
-                >
-                    <a
-                        href="#"
-                        className="block text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
-                    >
-                        Explore
-                    </a>
-                    <a
-                        href="#"
-                        className="block text-gray-500 hover:text-gray-700 px-3 font-normal py-2"
-                    >
-                        Advertise
-                    </a>
-                    {loginText}
-                    <a
-                        href="#"
-                        className="block text-gray-500 hover:text-gray-700 px-3 font-normal border-2 border-gray-400 hover:border-gray-800 rounded-lg py-2"
-                    >
-                        Submit a photo
-                    </a>
-                </div>
-            </div>
+        </div>
+    );
+
+    return (
+        <div>
+            {mainContent}
+            {responsiveContent}
         </div>
     );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
 import App from "./App.tsx";
 import Login from "./components/Login/Login.tsx";
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./components/PageNotFound/PageNotFound.tsx";
 
 const router = createBrowserRouter([
     { path: "/login", element: <Login /> },
-    { path: "/", element: <App /> },
+    { path: "/", element: <App />, errorElement: <ErrorPage /> },
 ]);
 
 export default router;
