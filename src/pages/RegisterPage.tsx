dd7c55a0ec7b88c7978edfe86480b30cb2cb3c4e
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-04-29 20:41:17 +0300

    Add message redirecting if register on not

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import SignUp from "../features/Registration/SignUp"
+
+function RegisterPage() {
+  return (
+    <div>
+        <SignUp/>
+    </div>
+  )
+}
+
+export default RegisterPage
--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,14 @@ import ErrorPage from "./pages/PageNotFound.tsx";
 import UploadImage from "./features/fileUpload/components/UploadImage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
 
 const authToken: String | null = localStorage.getItem("tokenAuth");
 
 const router = createBrowserRouter([
     { path: "/", element: <HomePage />, errorElement: <ErrorPage /> },
     { path: "/upload_image", element: <UploadImage /> },
+    { path: "/signupPage", element: <RegisterPage /> },
     {
         path: "/loginPage",
         element: !authToken ? <LoginPage /> : <Navigate to="/" />,
