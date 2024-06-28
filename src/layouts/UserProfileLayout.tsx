acef6018b33fccdecfe215358b37b61e2ac5e92f
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-06-28 22:04:37 +0300

    Make User profile page responsive

--- src/layouts/UserProfileLayout.tsx
+++ src/layouts/UserProfileLayout.tsx
@@ -6,41 +6,49 @@ import artImage from '../assets/images/art.jpg';
 import profilImage from'../assets/images/profile.png';
 
 const UserProfileLayout = () => {
+
+    const linkClassName ="text-gray-500 flex items-center gap-2 pb-4 border-b-2 border-white hover:text-black focus:text-black focus:border-b-2 focus:border-black focus:pb-4";
+
     return (
         <div>
-            <div className="px-44">
+            <div className="px-44 sm:px-6">
                 <div className="relative h-96 rounded-b">
                     <img className="h-96 w-full object-cover rounded-b" src={artImage} alt="cover"/>
                     <div className="absolute start-12 -bottom-20">
                         <img className="object-cover border-4 border-white w-40 h-40 rounded-full" src={profilImage} alt="" />
                     </div>
                 </div>
-            </div>
 
-            <div className="flex gap-5 mt-5 ms-5">
-                <div className="p-1">
-                    <Link to="" className="text-gray-500 flex items-center gap-2 hover:text-black focus:text-black focus:border-b-2 focus:border-black focus:pb-4">
-                        <FaHeart />
-                        like
-                    </Link>
+                <div className="mt-20 text-3xl font-bold">
+                    Linah Andrianarivony
                 </div>
-                <div className="p-1">
-                    <Link to="stats/" className="text-gray-500 flex items-center gap-2 hover:text-black focus:text-black focus:border-b-2 focus:border-black focus:pb-4">
-                        <IoMdStats />
-                        stats
-                    </Link>
-                </div>
-                <div className="p-1">
-                    <Link to="collections/" className="text-gray-500 flex items-center gap-2 hover:text-black focus:text-black focus:border-b-2 focus:border-black focus:pb-4">
-                        <MdCollections />
-                        collections
-                    </Link>
-                </div>
-            </div>
-            <hr />
 
-            <div className="flex justify-center mt-5">
-                <Outlet />
+                <div className="border border-gray-600 mt-6 border-opacity-10"></div>                
+                
+                <div className="flex gap-5 mt-5">
+                    <div className="p-1">
+                        <Link to="likes/" className={linkClassName}>
+                            <FaHeart />
+                            like
+                        </Link>
+                    </div>
+                    <div className="p-1">
+                        <Link to="stats/" className={linkClassName}>
+                            <IoMdStats />
+                            stats
+                        </Link>
+                    </div>
+                    <div className="p-1">
+                        <Link to="collections/" className={linkClassName}>
+                            <MdCollections />
+                            collections
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="flex justify-center mt-5">
+                    <Outlet />
+                </div>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'sm': {'max': '767px'}, // Point de rupture personnalisé pour les écrans en dessous de 768px
+            },
+        },
     },
     plugins: [],
 };
